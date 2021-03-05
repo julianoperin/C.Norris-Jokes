f89996fc77f717de6815d625fc38efe8d895d105
‘Juliano <julianoperins@gmail.com>
2021-03-05 13:44:54 -0500

    Started working on the random Joke

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Random = () => {
+  const [userInput, setUserInput] = useState("");
+
   return (
     <div>
-      <h1>random</h1>
+      <h1>Categories</h1>
+      <div className="box-category">
+        <input type="text" />
+        <select>
+          <option value="">animal</option>
+          <option value="">travel</option>
+          <option value="">food</option>
+        </select>
+        <button>Search</button>
+      </div>
     </div>
   );
 };
